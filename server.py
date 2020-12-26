f9b7a12df0b865b8290872ed93b99513a900b5ef
Eduard <mecozma@gmail.com>
2020-12-26 19:31:29 +0000

    Building a portfolio 4. Added a route to handle the POST/GET events

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'The form has been submitted.'
