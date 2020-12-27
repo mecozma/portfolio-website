021d0fde2bf09b2678509718f1589a0dd3524d95
Eduard <mecozma@gmail.com>
2020-12-27 18:36:13 +0000

    Added a file to save the form sent data.

--- server.py
+++ server.py
@@ -6,11 +6,20 @@ app = Flask(__name__)
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', 'a') as file:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file.write(f'\n{email}, {subject}, {message}')
+        
 # Route to handle the POST/GET events.
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong!'
