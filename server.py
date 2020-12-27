34f061eb8762808429103456107cf86fb22c4374
Eduard <mecozma@gmail.com>
2020-12-27 19:05:13 +0000

    Building a portfolio. Added a csv file to save the data sent by the form.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 
 app = Flask(__name__)
@@ -13,13 +14,23 @@ def write_to_file(data):
         subject = data['subject']
         message = data['message']
         file.write(f'\n{email}, {subject}, {message}')
-        
+
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as csv_file:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writter = csv.writer(csv_file, delimiter=',', quotechar='"',
+                                 quoting=csv.QUOTE_MINIMAL)
+        csv_writter.writerow([email, subject, message])
+
 # Route to handle the POST/GET events.
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong!'
